4dc89ed97dd2e90b277b52382ca04bd0456e45b3
elepantight <arvid1337@gmail.com>
2021-02-08 17:07:57 +0100

    responsive sidebar

--- src/component/nav.js
+++ src/component/nav.js
@@ -13,7 +13,7 @@ function Navigation() {
       <div className="navbar">
         <Link to="#" className="menu-bars">
           <img
-            className="menu_img"
+            className="menu-open"
             src={require("./../images/MENY.svg")}
             alt="img"
             onClick={showSidebar}
@@ -25,7 +25,7 @@ function Navigation() {
           <li className="navbar-toggle">
             <Link to="#" className="menu-bars">
               <img
-                className="menu_img"
+                className="menu-close"
                 src={require("./../images/MENYX.svg")}
                 alt="img"
               />
